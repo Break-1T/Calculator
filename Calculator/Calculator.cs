7f66346b87ef7e37f547c78c9aa8f92e0136838c
Break-1T <taras.krupko07@gmail.com>
2020-09-09 14:08:53 +0300

    Переместил класс Calculator в отдельное окно

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static string BinarySum(long x,long y)//111 + 10 = 1001
+        {
+            List<long> FirstNumbers = Digits(x);//111
+            List<long> SecondNumbers = Digits(y);//10
+            string result = "";
+
+            if (FirstNumbers.Count > SecondNumbers.Count)
+            {
+                SecondNumbers.Reverse();
+                int diff = FirstNumbers.Count - SecondNumbers.Count;
+                for (int i = 0; i < diff; i++)
+                    SecondNumbers.Add(0);
+                SecondNumbers.Reverse();
+            }
+            else
+            {
+                FirstNumbers.Reverse();
+                int diff = SecondNumbers.Count-FirstNumbers.Count;
+                for (int i = 0; i < diff; i++)
+                    FirstNumbers.Add(0);
+                FirstNumbers.Reverse();
+            }
+
+            int length = FirstNumbers.Count;
+            for(int i = length - 1; i >= 0; i--)
+            {
+                if ((FirstNumbers[i] + SecondNumbers[i]) > 1)
+                {
+                    result += "0";
+
+                }
+                result += (FirstNumbers[i] + SecondNumbers[i]).ToString();
+            }
+            result.Reverse();
+            return result;
+        }
+        private static List<long> Digits(long x)
+        {
+            List<long> input = new List<long>();
+            while (x > 0)
+            {
+                input.Add(x%10);
+                x /= 10;
+            }
+            input.Reverse();
+            return input;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,51 +11,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Calculator.BinarySum(10,10000));
-        }
-
-    }
-    class Calculator
-    {
-        public static string BinarySum(long x,long y)//111 + 10 = 1001
-        {
-            List<long> FirstNumbers = Digits(x);//111
-            List<long> SecondNumbers = Digits(y);//10
-            string result = "";
-            if (FirstNumbers.Count > SecondNumbers.Count)
-            {
-                SecondNumbers.Reverse();
-                int diff = FirstNumbers.Count - SecondNumbers.Count;
-                for (int i = 0; i < diff; i++)
-                    SecondNumbers.Add(0);
-                SecondNumbers.Reverse();
-            }
-            else
-            {
-                FirstNumbers.Reverse();
-                int diff = SecondNumbers.Count-FirstNumbers.Count;
-                for (int i = 0; i < diff; i++)
-                    FirstNumbers.Add(0);
-                FirstNumbers.Reverse();
-            }
-            int length = FirstNumbers.Count;
-            for(int i = length - 1; i >= 0; i--)
-            {
-
-            }
-
-            return "";
-        }
-        private static List<long> Digits(long x)
-        {
-            List<long> input = new List<long>();
-            while (x > 0)
-            {
-                input.Add(x%10);
-                x /= 10;
-            }
-            input.Reverse();
-            return input;
+            Console.WriteLine(Calculator.BinarySum(111,10));
         }
     }
 }
